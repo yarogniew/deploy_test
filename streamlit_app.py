8e07840b148a65f4943a59ff6fc3145993a423a6
yarogniew <yarogniew@dpoczta.pl>
2024-08-19 12:47:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
-st.write('Hello world!')
+st.set_page_config(
+    page_title="La mia App",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://dbdmg.polito.it/',
+        'Report a bug': "https://dbdmg.polito.it/",
+        'About': "# Corso di *Basi di Dati*"
+    }
+)
+st.title("📈 La mia App")
